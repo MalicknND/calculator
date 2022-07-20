3a9a403b7f5023a5af5401947b527ddf34d687cf
Malick NDIAYE <ndiayemalick062@gmail.com>
2022-07-20 16:25:53 +0200

    calculateur prêt

--- README.md
+++ README.md
@@ -13,3 +13,11 @@ Soustraction
 Division
 
 Nous utiliserons ce menu pour récupérer un nombre : 1, 2, 3 ou 4, qui nous permettra de savoir ce que l'utilisateur souhaite faire. Nous stockerons ce choix dans une variable choix.
+
+# deuxieme partie
+
+Dans cette deuxième étape, je vous invite à demander deux nombres à utiliser lors de vos calculs. Ces deux nombres seront stockés dans deux variables : premierNombre et deuxiemeNombre.
+
+Attention : Vérifiez bien qu'il s'agit de deux nombres, nous ne sommes pas à l'abri d'un utilisateur qui rentrerait du texte !
+
+Astuce : Pour vérifier si une valeur est bien un nombre, vous pouvez utiliser une boucle do...while, et utiliser la fonction isNaN(variable) qui renvoie true si la variable n'est pas un nombre.
--- exemple.js
+++ exemple.js
@@ -1,3 +1,23 @@
+function addition(nombreA, nombreB) {
+  let result = nombreA + nombreB;
+  return result;
+}
+function multiplication(nombreA, nombreB) {
+  let result = nombreA * nombreB;
+  return result;
+}
+function soustraction(nombreA, nombreB) {
+  let result = nombreA - nombreB;
+  return result;
+}
+function division(nombreA, nombreB) {
+  if (nombreB == 0) {
+    throw new Error('Vous ne pouvez pas diviser par 0.');
+  }
+  let result = nombreA / nombreB;
+  return result;
+}
+
 do {
   var choix = Number(
     prompt(
@@ -9,3 +29,31 @@ do {
   var premierNombre = Number(prompt('Entrer le premier nombre'));
   var deuxiemeNombre = Number(prompt('Entrer le deuxieme nombre'));
 } while (isNaN(premierNombre) || isNaN(deuxiemeNombre));
+
+try {
+  switch (choix) {
+    case 1:
+      var resultat = addition(premierNombre, deuxiemeNombre);
+      break;
+
+    case 2:
+      var resultat = multiplication(premierNombre, deuxiemeNombre);
+      break;
+
+    case 3:
+      var resultat = soustraction(premierNombre, deuxiemeNombre);
+      break;
+
+    case 4:
+      var resultat = division(premierNombre, deuxiemeNombre);
+
+      break;
+
+    default:
+      throw new Error('Une erreur est survenue');
+  }
+
+  alert('Voici le résultat ' + resultat);
+} catch (error) {
+  alert(error);
+}
